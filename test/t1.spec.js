d77f6798e1e980b5e0c304bc225812cfc77bc37a
AndriyNikiforov <andriy.nikiforov.general@gmail.com>
2019-02-26 16:46:41 +0200

    Add new docker.

--- test/t1.spec.js
+++ test/t1.spec.js
@@ -1,27 +1,17 @@
 const { Builder, By, until, Options } = require('./config');
 
-const firefox =  new Builder()
-.forBrowser('firefox')
-.usingServer('http://192.168.10.94:4444/wd/hub')
-.withCapabilities({
-  browserName: 'firefox',
-  webStorageEnabled: false,
-  locationContextEnabled: false,
-  screenResolution: '1280x720'
-})
-.build();
-
 const chrome = new Builder()
   .forBrowser('chrome')
-  .usingServer('http://192.168.10.94:4444/wd/hub')
+  .usingServer('http://localhost:4444/wd/hub')
   .withCapabilities({
     browserName: 'chrome',
     webStorageEnabled: false,
     locationContextEnabled: false,
-    screenResolution: '1280x720'
+    screenResolution: '640x480'
   })
   .setChromeOptions(new Options().addArguments([
-     '--fast',
+    '--fast',
+    '--no-sandbox',
     '--fast-start',
     '--disable-gpu',
     '--disable-cache',
@@ -38,21 +28,18 @@ const chrome = new Builder()
   ]))
   .build();
 
+jest.setTimeout(200000);
+
 describe('t1', () => {
-  beforeAll(async () => {
-    await jest.setTimeout(200000);
-    await chrome.get('https://google.com');
-    await firefox.get('https://www.seleniumeasy.com/test/basic-radiobutton-demo.html');
-  });
+  beforeAll(async () => await chrome.get('https://google.com'));
 
   it('google', async () => {
-    await chrome.wait(until
-      .elementLocated(By.name('q')), 5)
+    await chrome.findElement(By.name('q'))
       .sendKeys('golang');
 
     await chrome.wait(until
         .elementLocated(By
-            .css('li.sbct:first-child'), 5))
+            .css('li.sbct:first-child'), 0.1))
        .click();
 
     await chrome.findElement(By.className('LC20lb'))
@@ -70,20 +57,21 @@ describe('t1', () => {
 
     await chrome.wait(until
       .elementLocated(By
-        .css('div.container div.left:nth-child(2) div:nth-child(1) div.buttons > a.run')), 5)
+        .css('div.container div.left:nth-child(2) div:nth-child(1) div.buttons > a.run')), 0.1)
       .click();
   });
 
   it('t2', async () => {
-    await firefox.findElement(By.css('input[value="Male"]'))
+    await chrome.get('https://www.seleniumeasy.com/test/basic-radiobutton-demo.html');
+    await chrome.findElement(By.css('input[value="Male"]'))
       .click();
 
-    await firefox.findElement(By.id('buttoncheck')).click();
-    await until.elementTextContains(firefox
+    await chrome.findElement(By.id('buttoncheck')).click();
+    await until.elementTextContains(chrome
       .findElement(By
         .className('radiobutton')), 'Radio button is Not checked');
 
-    await firefox.findElement(By
+    await chrome.findElement(By
       .css('div.panel.panel-default:nth-child(5) div.panel-body > button.btn.btn-default:nth-child(5)'))
       .click();
   });
@@ -91,6 +79,5 @@ describe('t1', () => {
   afterAll( async done => {
     await done();
     await chrome.quit();
-    await firefox.quit();
   });
 });
